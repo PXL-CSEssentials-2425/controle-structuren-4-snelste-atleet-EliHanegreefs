24b1d7de2485afc2f7ec0021a4264edbfafa79d2
EliHanegreefs <eli.hanegreefs@stubent.pxl.be>
2024-10-04 12:27:37 +0200

    klaar

--- snelste_atleet/MainWindow.xaml.cs
+++ snelste_atleet/MainWindow.xaml.cs
@@ -16,9 +16,48 @@ namespace snelste_atleet
     /// </summary>
     public partial class MainWindow : Window
     {
+        string nameFastest;
+        int secondsFastest = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this .Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            secondenTextBox.Clear();
+            resultTextBox.Clear();
+
+            nameTextBox.Focus();
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValidNumber = int.TryParse(secondenTextBox.Text, out int secondsCurrent);
+            //int secondsCurrent;
+
+            if (isValidNumber)
+            {
+                if (secondsFastest == 0 || secondsFastest < secondsFastest)
+                {
+                    secondsFastest = secondsCurrent;
+                    nameFastest = nameTextBox.Text;
+                }
+                nameTextBox.Clear();
+                secondenTextBox .Clear();
+                nameTextBox .Focus();
+            }
+        }
+
+        private void fastestButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Text = $"De snelste atleet is {nameFastest} met een tijd van {secondsFastest} seconden";
         }
     }
 }
